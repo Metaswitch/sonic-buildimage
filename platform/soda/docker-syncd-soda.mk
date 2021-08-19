1b0ba27fd995cbeec0a24da61d7d97364abc321b
Tom cappleman <thomas.cappleman@metaswitch.com>
2021-08-19 17:29:52 +0100

    SODA repo and new files

---
+++ platform/soda/docker-syncd-soda.mk
@@ -0,0 +1,14 @@
+# docker image for soda syncd
+
+DOCKER_SYNCD_PLATFORM_CODE = soda
+include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+
+$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
+
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
+                                $(LIBSWSSCOMMON_DBG) \
+                                $(LIBSAIMETADATA_DBG) \
+                                $(LIBSAIREDIS_DBG)
+
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /var/log/syncd:/var/log/syncd:rw
